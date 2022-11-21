76810a89a93474c67f937c153a59f469bcbabced
Franssen <ngoiefranssen04@gmail.com>
2022-11-21 16:52:27 +0200

    Wrapper App

---
+++ src/components/Header/Hearder.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    
+    return(
+        <div className="header">
+            <h1 className="header_title">Forecast Finder</h1>
+        </div>
+    );
+};
+
+export default Header;
