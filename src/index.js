b3f7b30730eaed56556890e722cc18f7ff069512
Franssen <ngoiefranssen04@gmail.com>
2022-11-21 17:06:42 +0200

    Wrapper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/styles.scss';
+// import './styles/styles.scss';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
