6073ab878d31584568e59730e9c252bfdf2b34b7
Franssen <ngoiefranssen04@gmail.com>
2022-11-22 15:42:29 +0200

    Wrapper

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import axios from "axios";
 import Header from "../Header/Hearder";
 
@@ -7,13 +7,20 @@ const API_KEY = '9eac7e06a6b320e47fe2dde7d5dd104d';
 
 
 const Main = () => {
-    const  api_cal = async () =>
-    {
+
+    const  api_data = async () =>{
+        
         const urlData = `https://api.openweathermap.org/data/2.5/weather?q=Liverpool&appid=${API_KEY}`;
         const resquestDadta = axios.get(urlData);
 
-        const reponseData = await resquestDadta;
-    }
+        const responseData = await resquestDadta;
+        console.log(responseData)
+    };
+
+    useEffect(() =>{
+        api_data();
+    }, []);
+
     return (
         <div className="main">
             <Header />
